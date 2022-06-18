ec132fd1f86f0259a61ab2a6aa897eb9c8f7c51d
raghadalharbi396 <raghad.alharbi396@gmail.com>
2022-06-19 02:19:47 +0300

    add addBlock and vaildateBlockchsin

--- Block.js
+++ Block.js
@@ -15,5 +15,21 @@ calculateHash(){
     this.haight + this.timestamp + this.data + this.prevHash
   ).toString();
 }
+/*maining(){
+    difficulty = 4;
+    timetry = 0;
+    j = 10-0000-000;
+    for( i=0 ; i < j; i++){
+        magichash = this.calculateHash();
+    console.log(magichash);
+    if(magichash.subString(0,difficulty ) == "0000"){
+        console.log("magichash:"+ magichash);
+       console.log("time trying:"+timetry);
+        return;
+    }
+    timetry++;
+    nonce++;
+    }
+  }*/
 }
 export default Block;
--- Blockchain.js
+++ Blockchain.js
@@ -1,7 +1,7 @@
-import Block from "./Block";
+import Block from "./Block.js";
 
 class Blockchain {
-    chain;
+   chain ;
     constructor(){
         this.chain = [];
     }
@@ -10,6 +10,28 @@ init(){
         return;
     }
 this.chain =new  Block(0 , Date.now , "new block" , 0 ,"");
+}
+
+  getLastBlock(){
+    return this.chain[this.Chain.length -1];
+  }
+  addBlock(data){
+    PrevHash = this.getLastBlock.hash;
+    oldHaight = this.getLastBlock.haight;
+    this.chain = new Block(oldHaight+1 ,Date.now,data,0,PrevHash);
+  }
+vaildateBlockchsin(){
+    for(let i= 0; i<this.chain.length ; i++){
+        current = this.chain[i];
+        next = this.chain[i+1];
+        if(current.hash != current.calculateHash()){
+            return false;
+        }
+        if(current.hash != next.PrevHash){
+            return false;
+        }
+
+    } return true;
 }
 }
 export default Blockchain;
